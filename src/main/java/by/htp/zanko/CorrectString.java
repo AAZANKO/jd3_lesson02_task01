3eb0ed02500622970142da2f810a143b628b0df0
AAZANKO <aleksandr.zan87@gmail.com>
2019-03-01 17:30:08 +0300

    one to commit

---
+++ src/main/java/by/htp/zanko/CorrectString.java
@@ -0,0 +1,55 @@
+package by.htp.zanko;
+
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@NoArgsConstructor
+public class CorrectString {
+
+    public boolean getCorrectString(String str) {
+
+        List<Character> massiveChar = new ArrayList<>();
+
+        for (int i = 0; i < str.length(); i++) {
+
+            if (i == 0) {
+                if (str.charAt(i) == ')' || str.charAt(i) == ']' || str.charAt(i) == '}') {
+                    return false;
+                }
+            }
+
+            if (str.charAt(i) == '(') {
+                massiveChar.add(')');
+            } else if (str.charAt(i) == ')') {
+                if (massiveChar.get(massiveChar.size() - 1) == ')') {
+                    massiveChar.remove(massiveChar.size() - 1);
+                } else {
+                    return false;
+                }
+            }
+            if (str.charAt(i) == '[') {
+                massiveChar.add(']');
+            } else if (str.charAt(i) == ']') {
+                if (massiveChar.get(massiveChar.size() - 1) == ']') {
+                    massiveChar.remove(massiveChar.size() - 1);
+                } else {
+                    return false;
+                }
+            }
+
+            if (str.charAt(i) == '{') {
+                massiveChar.add('}');
+            } else if (str.charAt(i) == '}') {
+                if (massiveChar.get(massiveChar.size() - 1) == '}') {
+                    massiveChar.remove(massiveChar.size() - 1);
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+}
---
+++ src/test/java/by/htp/zanko/CorrectStringTest.java
@@ -0,0 +1,16 @@
+package by.htp.zanko;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CorrectStringTest {
+
+    private CorrectString correctString = new CorrectString();
+
+    @Test
+    public void checkGetCorrectString() {
+
+        boolean resultCorrectString = correctString.getCorrectString("hi123 (yg [uog (vrd) ]5)674 (gio h87y) hui {n hg768} ");
+        Assert.assertNotNull(resultCorrectString);
+    }
+}
